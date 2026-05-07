ea290050df3ee0c15ddea1e1aed2dd89094fee29
Rickaiva <pradoricka@gmail.com>
2026-05-08 07:01:26 +0930

    8/5/26

--- Portfolio website/index.js
+++
@@ -1,9 +0,0 @@
-const head = document.getElementById("head");
-const hand = document.getElementById("hand");
-const building = document.getElementById("building");
-
-document.addEventListener("scroll", () => {
-    let value = window.scrollY;
-    head.style.top = value * 0.4 + "px";
-
-})
